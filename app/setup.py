b42ae5b32f31440e7887f1f1e2bcbae2e3a77995
cbourget <charlesericbourget@gmail.com>
2020-04-10 21:52:36 -0400

    Another attempt

--- app/setup.py
+++ app/setup.py
@@ -3,9 +3,11 @@ import os
 from setuptools import setup, find_packages
 
 name = 'gero.app'
+
 here = os.path.dirname(os.path.realpath(__file__))
-packages = [name] + find_packages(
-    '{}/{}'.format(here, name.replace('.', '/')))
+packages = [name]
+for subpackage in find_packages('{}/{}'.format(here, name.replace('.', '/'))):
+    packages.append('{}.{}'.format(name, subpackage))
 
 install_requires = [
     'capri.utils',
