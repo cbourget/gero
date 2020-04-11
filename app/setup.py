78c8f35bd9b9cf6d3e44094aecc3fead2b9f7a77
cbourget <charlesericbourget@gmail.com>
2020-04-10 21:32:21 -0400

    Another attempt

--- app/setup.py
+++ app/setup.py
@@ -1,9 +1,4 @@
-import pip
-import os
-
 from setuptools import setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
 
 
 name = 'gero.app'
@@ -16,36 +11,6 @@ install_requires = [
     'psycopg2'
 ]
 
-def install_packages(packages, develop=False):
-    """ Use pip to install all libraries.  """
-
-    wd = os.getcwd()
-    for package in packages:
-        os.chdir(os.path.join(wd, package))
-
-        if develop:
-            pip.main(['install', '-e', '.'])
-        else:
-            pip.main(['install', '.'])
-
-        os.chdir(wd)
-
-
-class DevelopCmd(develop):
-    """ Add custom steps for the develop command """
-
-    def run(self):
-        install_packages(packages, develop=True)
-        develop.run(self)
-
-
-class InstallCmd(install):
-    """ Add custom steps for the install command """
-
-    def run(self):
-        install_packages(packages, develop=False)
-        install.run(self)
-
 setup(
     name=name,
     version='0.1.0',
@@ -57,10 +22,12 @@ setup(
         'Private :: Do Not Upload to pypi server'
     ],
     namespace_packages=['gero'],
-    packages=[name],
-    install_requires=install_requires,
-    cmdclass={
-        'install': InstallCmd,
-        'develop': DevelopCmd
-    }
+    packages=[
+        name,
+        'gero.app.contexts',
+        'gero.app.database',
+        'gero.app.domain',
+        'gero.app.utils'
+    ],
+    install_requires=install_requires
 )
