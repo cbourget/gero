a42e0b99168e2349481c894c3f3fdc896add3ac7
cbourget <charlesericbourget@gmail.com>
2020-04-10 21:17:01 -0400

    Another attempt at fixing missing file when pip installing

--- app/setup.py
+++ app/setup.py
@@ -1,4 +1,9 @@
+import pip
+import os
+
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 
 name = 'gero.app'
@@ -10,6 +15,36 @@ install_requires = [
 
     'psycopg2'
 ]
+
+def install_packages(packages, develop=False):
+    """ Use pip to install all libraries.  """
+
+    wd = os.getcwd()
+    for package in packages:
+        os.chdir(os.path.join(wd, package))
+
+        if develop:
+            pip.main(['install', '-e', '.'])
+        else:
+            pip.main(['install', '.'])
+
+        os.chdir(wd)
+
+
+class DevelopCmd(develop):
+    """ Add custom steps for the develop command """
+
+    def run(self):
+        install_packages(packages, develop=True)
+        develop.run(self)
+
+
+class InstallCmd(install):
+    """ Add custom steps for the install command """
+
+    def run(self):
+        install_packages(packages, develop=False)
+        install.run(self)
 
 setup(
     name=name,
@@ -23,5 +58,9 @@ setup(
     ],
     namespace_packages=['gero'],
     packages=[name],
-    install_requires=install_requires
+    install_requires=install_requires,
+    cmdclass={
+        'install': InstallCmd,
+        'develop': DevelopCmd
+    }
 )
